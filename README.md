8f9fad49454fcc3c1b614ca659e82b7bce505bc3
Yankue <tpower@highstorrs.sheffield.sch.uk>
2021-03-21 11:59:28 +0000

    Uploaded

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # oauth2-example
 A very easy way to do discord oauth2 in JavaScript!
+
+## Usage
+1) In main.js, put your credentials in, on lines 7-12
+2) On the developer portal, set `http://localhost:3000/oauth-callback` to an OAuth2 redirect. Change the url/port how you wish.
+
+This is clearly an example, but it shows exactly how to use the identify scope on Discord OAuth2.
---
+++ main.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const { join } = require('path');
+const app = express();
+const apiBase = "https://discord.com/api";
+
+const config = {
+    CLIENT_ID: "820648030754832464",
+    CLIENT_SECRET: "Gl7UzcwTF1sG6MAv-ueC94QX6xJ80Lgt",
+    PORT: 3000
+ }
+config.REDIRECT_URI = `http://localhost:${config.PORT}/oauth-callback`;
+
+app.get('/', async (req, res) => {
+    res.sendFile(join(__dirname + "/homepage.html"));
+});
+
+app.get('/login', async (req, res) => {
+    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${config.CLIENT_ID}&redirect_uri=` + encodeURIComponent(`${config.REDIRECT_URI}`) + `&response_type=code&scope=identify`);
+});
+
+app.get('/oauth-callback', async (req, res) => {
+    const code = req.query.code;
+    if(!code) return res.redirect('http://localhost:' + config.port + "/login");
+    fetch(`${apiBase}/oauth2/token`, {
+        method: "POST",
+        headers: {
+           "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+           "client_id": config.CLIENT_ID,
+           "client_secret": config.CLIENT_SECRET,
+           "grant_type": "authorization_code",
+           "code": code,
+           "redirect_uri": config.REDIRECT_URI,
+           "scope": "identify guilds email"
+        })
+    }).then(async (result) => {
+        result = await result.json();
+
+        const data = await fetch(`${apiBase}/v6/users/@me`, {
+            method: "GET",
+            headers: {
+               "Authorization": `Bearer ${result.access_token}`
+            }
+         })
+         const completeData = await data.json();
+         res.send(completeData);
+    })
+});
+
+app.listen(config.PORT);
